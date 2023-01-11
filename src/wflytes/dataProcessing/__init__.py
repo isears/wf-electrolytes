330967c2c978349bfdf3982c0ff4453d96456bce
isears <isaac.j.sears@gmail.com>
2023-01-11 16:31:26 -0500

    Continue work on preprocessor

--- src/wflytes/dataProcessing/__init__.py
+++ src/wflytes/dataProcessing/__init__.py
@@ -1,7 +1,9 @@
+import glob
 import os
 import pickle
+import shutil
 from dataclasses import dataclass
-from typing import List
+from typing import Generator
 
 import numpy as np
 import pandas as pd
@@ -18,6 +20,9 @@ potassium_events = [
 
 @dataclass
 class HadmWfRecord:
+    """
+    Designed to be lazy for minimal memory footprint when used with dataset
+    """
     hadm_id: int
     instantation_record: wfdb.Record = None
 
@@ -25,13 +30,62 @@ class HadmWfRecord:
         self._path = f"data/{self.hadm_id}"
         assert os.path.exists(self._path)
 
+    def _load_metadata(self) -> dict:
+        with open(f"{self._path}/metadata.pkl", 'rb') as f:
+            metadata = pickle.load(f)
+
+        return metadata
+
+    def update_metadata(self, updates: dict[any, any]) -> None:
+        metadata = self._load_metadata()
+
+        for k, v in updates.items():
+            metadata[k] = v
+
+        with open(f"{self._path}/metadata.pkl", 'wb') as f:
+            pickle.dump(metadata, f)
+
+    def get_signal_names(self) -> list[str]:
+        return self._load_metadata()['sig_name']
+
+    def generate_signals(self) -> Generator[tuple[str, np.ndarray], None, None]:
+        for signal_name in self.get_signal_names():
+            yield signal_name, np.load(f"{self._path}/{signal_name}.npy")
+
     def has_signal(self, signal_name: str) -> bool:
-        raise NotImplementedError()
+        return signal_name in self._load_metadata()['sig_name']
 
-    def get_signal(self, signal_name: str) -> np.ndarray:
-        raise NotImplementedError()
+    def get_full_signal(self, signal_name: str) -> np.ndarray:
+        if not os.path.exists(f"{self._path}/{signal_name}.npy"):
+            raise ValueError(f"{signal_name} signal not found for hadm id {self.hadm_id}")
 
-    def get_signals(self, signal_names: List[str]) -> np.ndarray:
+        return np.load(f"{self._path}/{signal_name}.npy")
+
+    def get_segment_count(self, signal_name: str) -> int:
+        if not os.path.exists(f"{self._path}/processed/{signal_name}"):
+            return 0
+        
+        return len(glob.glob(f"{self._path}/processed/{signal_name}/*.npy"))
+
+    def get_segment(self, signal_name: str, idx: int) -> np.ndarray:
+        all_segments = glob.glob(f"{self._path}/processed/{signal_name}/*.npy")
+        return np.load(all_segments[idx])
+
+    def cache_segment(self, signal_name: str, signal: np.ndarray, start_idx: int, end_idx: int):
+        if not os.path.exists(f"{self._path}/processed/{signal_name}"):
+            os.makedirs(f"{self._path}/processed/{signal_name}")
+
+        segment = signal[start_idx: end_idx]
+        np.save(f"{self._path}/processed/{signal_name}/{start_idx}_{end_idx}", segment)
+
+    def clean_processed_cache(self) -> None:
+        if os.path.exists(f"{self._path}/processed"):
+            assert ".." not in self._path, "[-] Uhhhhhh...."
+            shutil.rmtree(f"{self._path}/processed")
+
+        os.makedirs(f"{self._path}/processed")
+        
+    def get_signals(self, signal_names: list[str]) -> np.ndarray:
         raise NotImplementedError()
 
     @staticmethod
--- src/wflytes/dataProcessing/preprocess.py
+++ src/wflytes/dataProcessing/preprocess.py
@@ -4,40 +4,48 @@ into more manageable pieces
 """
 import datetime
 import glob
-import os
-import pickle
-import shutil
+import multiprocessing
 
 import numpy as np
+from tqdm import tqdm
+
+from wflytes.dataProcessing import HadmWfRecord
 
 
-def handle_single_dir(dirname, duration: datetime.timedelta, min_variance: float = 0.1):
-    if os.path.exists(f"{dirname}/processed"):
-        assert ".." not in dirname, "[-] Uhhhhhh...."
-        shutil.rmtree(f"{dirname}/processed")
-
-    os.makedirs(f"{dirname}/processed")
-
-    with open(f"{dirname}/metadata.pkl", "rb") as f:
-        metadata = pickle.load(f)
+def handle_single_hadm(
+    hadm_id: int,
+    duration: datetime.timedelta = datetime.timedelta(seconds=60),
+    min_variance: float = 0.1,
+):
+    record = HadmWfRecord(hadm_id)
+    record.clean_processed_cache()
+    metadata = record._load_metadata()
 
     desired_length = int(metadata["fs"] * duration.total_seconds())
 
-    for sig_file in glob.glob(f"{dirname}/*.npy"):
-        sig = np.load(sig_file)
+    for sig_name, sig in record.generate_signals():
+        sig = np.nan_to_num(sig, nan=0.0)
 
-        window_size = 20  # Can't do full desired_length b/c it will run out of mem
-        stride = sig.strides[0]
-        window_shape = (sig.shape[0] - window_size + 1, window_size)
-        sliding_window_view = np.lib.stride_tricks.as_strided(
-            sig, window_shape, (stride, stride)
-        )
-        variances = np.var(sliding_window_view, axis=1)
-        variances = variances[: sig.shape[0] - desired_length]
-        valid_indices = np.where(variances > min_variance)[0]
+        # pad array up to size that's divisible by desired_length
+        pad_size = int(np.ceil(sig.size / desired_length) * desired_length) - sig.size
+        sig = np.pad(sig, (0, pad_size))
 
-        print("deleteme")
+        # TODO: sliding window may get us more data, but is much more time-consuming
+        sig = sig.reshape(int(sig.size / desired_length), desired_length)
+
+        segment_variances = np.var(sig, axis=1)
+
+        for idx, var in enumerate(segment_variances):
+            if var > min_variance:
+                record.cache_segment(
+                    sig_name, sig, idx * desired_length, idx + desired_length
+                )
+
+    record.update_metadata({"duration": duration})
 
 
 if __name__ == "__main__":
-    handle_single_dir("data/189051", datetime.timedelta(seconds=60))
+
+    for dirname in tqdm(glob.glob("data/*")):
+        hadm_id = int(dirname.split("/")[-1])
+        handle_single_hadm(hadm_id, datetime.timedelta(seconds=60))
